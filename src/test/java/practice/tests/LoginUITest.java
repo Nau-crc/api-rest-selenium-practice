288fdfbe980eb67815d59e24ec2b14f4d426f714
Gabi <gabi@Xaviers-MacBook-Pro.local>
2025-02-12 10:07:02 +0100

    ASAR-1 refactoring

--- src/test/java/practice/tests/LoginUITest.java
+++ src/test/java/practice/tests/LoginUITest.java
@@ -16,15 +16,13 @@ public class LoginUITest extends BaseTest {
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
 
-       loginPage.login("TestUser", "Password!1");
+        loginPage.login("TestUser", "Password!1");
 
         String token = ApiHelper.loginAndGetToken("TestUser", "Password!1");
 
         Assert.assertNotNull(token, "El token no debe ser nulo");
-       
         Assert.assertEquals(homePage.userNameLogged(), "TestUser");
         
-   
     }
 }
 
